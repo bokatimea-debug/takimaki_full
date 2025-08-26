f1987ce0211e7d1d992b1a96909cd8d06d11f1bd
bokatimea-debug <bokatimea@gmail.com>
2025-08-26 19:18:30 +0200

    main: routes + RoleSelectScreen as home

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,7 @@
-// lib/main.dart
 import 'package:flutter/material.dart';
-
-import 'utils.dart';
 import 'screens/role_select_screen.dart';
 import 'screens/customer_search_screen.dart';
 import 'screens/provider_profile_setup_screen.dart';
-import 'screens/home.dart'; // ha hivatkozol rá máshol
 
 void main() => runApp(const TakimakiApp());
 
@@ -14,24 +10,16 @@ class TakimakiApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final colorScheme = ColorScheme.fromSeed(seedColor: takimakiTurquoise);
+    final colorScheme = ColorScheme.fromSeed(seedColor: const Color(0xFF00B8B8));
     return MaterialApp(
-      title: 'Takimaki',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: colorScheme.copyWith(
-          primary: takimakiTurquoise,
-          secondary: takimakiOrange,
-        ),
-        useMaterial3: true,
-      ),
-      home: RoleSelectScreen(),
+      title: 'Takimaki',
+      theme: ThemeData(useMaterial3: true, colorScheme: colorScheme),
+      home: const RoleSelectScreen(),
       routes: {
         '/customer/search': (context) => const CustomerSearchScreen(),
         '/provider/setup': (context) => const ProviderProfileSetupScreen(),
-        '/home': (context) => const HomeScreen(),
       },
     );
   }
 }
-
