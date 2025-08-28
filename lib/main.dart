d3d4d209c1c0dfe0abed599ac77e1e93e43c7ae4
bokatimea-debug <bokatimea@gmail.com>
2025-08-28 21:01:03 +0200

    reg: app starts at splash login (Google/Apple buttons)

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import 'screens/registration_screen.dart';
-import 'screens/role_select_screen.dart';
+import 'screens/splash_login_screen.dart';
 
 void main() {
   runApp(const TakimakiApp());
@@ -19,34 +17,7 @@ class TakimakiApp extends StatelessWidget {
         colorSchemeSeed: const Color(0xFF0FA3A9),
         useMaterial3: true,
       ),
-      home: const _Bootstrap(),
-    );
-  }
-}
-
-class _Bootstrap extends StatefulWidget {
-  const _Bootstrap();
-
-  @override
-  State<_Bootstrap> createState() => _BootstrapState();
-}
-
-class _BootstrapState extends State<_Bootstrap> {
-  Future<bool> _load() async {
-    final sp = await SharedPreferences.getInstance();
-    return sp.getBool(RegistrationScreen.prefKey) ?? false;
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return FutureBuilder<bool>(
-      future: _load(),
-      builder: (c, s) {
-        if (!s.hasData) {
-          return const Scaffold(body: Center(child: CircularProgressIndicator()));
-        }
-        return s.data! ? const RoleSelectScreen() : const RegistrationScreen();
-      },
+      home: const SplashLoginScreen(),
     );
   }
 }
