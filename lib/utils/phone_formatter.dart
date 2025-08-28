235faffac23b147bdedcf14fd1419c2ad3fc5f7e
bokatimea-debug <bokatimea@gmail.com>
2025-08-28 20:39:59 +0200

    reg: HU phone formatter (+36 …) with free typing after prefix

--- lib/utils/phone_formatter.dart
+++ lib/utils/phone_formatter.dart
@@ -4,31 +4,28 @@ class HuPhoneFormatter extends TextInputFormatter {
   @override
   TextEditingValue formatEditUpdate(
       TextEditingValue oldValue, TextEditingValue newValue) {
-    var text = newValue.text;
-
-    // csak egy példa: ha +36-tal kezdődik, rakjuk szokásos szóközöket
-    if (text.startsWith('+36')) {
-      final digits = text.replaceAll(RegExp(r'\D'), '');
-      // +36 (2) (3) (4) => +36 20 123 4567 mintázat
-      String out = '+36';
-      var rest = digits.substring(3); // 36 után
-      if (rest.isNotEmpty) {
-        if (rest.length <= 2) {
-          out += ' $rest';
-        } else if (rest.length <= 5) {
-          out += ' ${rest.substring(0,2)} ${rest.substring(2)}';
-        } else if (rest.length <= 8) {
-          out += ' ${rest.substring(0,2)} ${rest.substring(2,5)} ${rest.substring(5)}';
-        } else {
-          out += ' ${rest.substring(0,2)} ${rest.substring(2,5)} ${rest.substring(5,9)}';
-        }
-      }
-      return TextEditingValue(
-        text: out,
-        selection: TextSelection.collapsed(offset: out.length),
-      );
+    final raw = newValue.text;
+    if (!raw.startsWith('+36')) {
+      const fixed = '+36 ';
+      return TextEditingValue(text: fixed,
+          selection: const TextSelection.collapsed(offset: fixed.length));
     }
-
-    return newValue;
+    final digits = raw.replaceAll(RegExp(r'[^\d]'), '');
+    if (!digits.startsWith('36')) {
+      const fixed = '+36 ';
+      return TextEditingValue(text: fixed,
+          selection: const TextSelection.collapsed(offset: fixed.length));
+    }
+    String out = '+36';
+    String rest = digits.substring(2);
+    if (rest.isEmpty) out += ' ';
+    else if (rest.length <= 2) out += ' ${rest.substring(0)}';
+    else if (rest.length <= 5) out += ' ${rest.substring(0,2)} ${rest.substring(2)}';
+    else if (rest.length <= 9) out += ' ${rest.substring(0,2)} ${rest.substring(2,5)} ${rest.substring(5)}';
+    else out += ' ${rest.substring(0,2)} ${rest.substring(2,5)} ${rest.substring(5,9)}';
+    return TextEditingValue(
+      text: out,
+      selection: TextSelection.collapsed(offset: out.length),
+    );
   }
 }
