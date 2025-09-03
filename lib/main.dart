c71df9790fe329ccd6a99429e56a04112476ee6e
bokatimea-debug <bokatimea@gmail.com>
2025-09-03 22:54:39 +0200

    chore: call seedTestUser() in debug mode before runApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-﻿import 'screens/provider_offer_reply_screen.dart';
+import 'screens/provider_offer_reply_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
